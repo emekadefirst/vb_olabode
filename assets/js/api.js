7f0de02e767c36c2a352186a4ef68a367b88a8f6
emekadefirst <victorchibuogwu33@gmail.com>
2024-10-22 01:19:36 +0100

    { ./api } changed

--- assets/js/api.js
+++ assets/js/api.js
@@ -6,6 +6,6 @@
 //https://verbumdei-management-system-vms.onrender.com
 
 // const api = "http://127.0.0.1:8000";
-// module.exports = http://service.verbumdeiportal.com
+// module.exports = https://service.verbumdeiportal.com
 export const api = "https://verbumdei-management-system-vms.onrender.com";
 
